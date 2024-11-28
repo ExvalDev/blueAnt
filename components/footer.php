102024fe0f81bb01b41ab17d2ab2f77dc937472a
Christian Koch <c.koch@exval.de>
2024-11-28 16:10:57 +0100

    Set Version to 1.0

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer p-3">
-    <p>&copy; <?php echo date('Y'); ?> HTW [Gruppe 5]</p>
+    <p>&copy; <?php echo date('Y'); ?> HTW [Gruppe 5] | Version 1.0</p>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/custom.js"></script>
