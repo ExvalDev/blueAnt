6d69df175320a978e738acfcc2a9effa557a3c15
Philipp <philipp.till99@win.tu-berlin.de>
2025-01-10 12:08:36 +0100

    änderungen an detailsicht

--- utilities/helpers.php
+++ utilities/helpers.php
@@ -55,3 +55,11 @@ if (!function_exists("searchCustomFieldByName")) {
         return null;
     }
 }
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
